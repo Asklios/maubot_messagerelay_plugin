8670e3e4d270934f3dd065b47cb9e6bf4e47974a
Asklios <42278434+Asklios@users.noreply.github.com>
2021-11-07 23:35:20 +0100

    ws auto reconnect

--- messagerelay/messagerelay.py
+++ messagerelay/messagerelay.py
@@ -3,7 +3,7 @@ from maubot.handlers import command, event
 from mautrix.types import MessageEvent, RoomID, MessageType, EventType, StateEvent
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 from typing import Type
-from websockets import connect
+from websockets import connect, ConnectionClosed
 import asyncio
 import json
 
@@ -39,50 +39,54 @@ class Messagerelay(Plugin):
         if api_key == "" or api_uri == "":
             self.log.error("API key or uri is not set!")
         else:
-            async with connect(api_uri) as websocket:
-                data = {'type': 'code', 'code': api_key}
-                await websocket.send(json.dumps(data))
+            async for websocket in connect(api_uri,):
+                try:
+                    data = {'type': 'code', 'code': api_key}
+                    await websocket.send(json.dumps(data))
 
-                while True:
-                    r: json = json.loads(await websocket.recv())
+                    while True:
+                        r: json = json.loads(await websocket.recv())
 
-                    if r.get('type') == 'verified':
-                        self.log.debug(f'connected to websocket at {api_uri}')
-                    elif r.get('type') == 'error':
-                        self.log.error(f'[ERROR]: {r.get("msg")}')
-                    elif r.get('type') == 'create':
-                        # self.log.info(f'new message ({r.get("id")}) to {r.get("target")}: {r.get("content")}')
+                        if r.get('type') == 'verified':
+                            self.log.debug(f'connected to websocket at {api_uri}')
+                        elif r.get('type') == 'error':
+                            self.log.error(f'[ERROR]: {r.get("msg")}')
+                        elif r.get('type') == 'create':
+                            # self.log.info(f'new message ({r.get("id")}) to {r.get("target")}: {r.get("content")}')
 
-                        room_name = r.get("target")
-                        room_id: RoomID = RoomID(self.db.get_room_id(room_name)[2])
+                            room_name = r.get("target")
+                            room_id: RoomID = RoomID(self.db.get_room_id(room_name)[2])
 
-                        if room_id is None:
-                            self.log.debug(f"Target '{room_name}' is not mapped to room.")
-                        else:
-                            content = r.get("content")
-                            # self.log.info(f"Sending message to {room_id}")
-                            msg_event_id = await self.client.send_markdown(room_id=room_id,
-                                                                           markdown=content,
-                                                                           allow_html=True,
-                                                                           msgtype=MessageType.TEXT
-                                                                           )
-                            self.db.save_message(room_name, room_id, msg_event_id, r.get("id"), content)
+                            if room_id is None:
+                                self.log.debug(f"Target '{room_name}' is not mapped to room.")
+                            else:
+                                content = r.get("content")
+                                # self.log.info(f"Sending message to {room_id}")
+                                msg_event_id = await self.client.send_markdown(room_id=room_id,
+                                                                               markdown=content,
+                                                                               allow_html=True,
+                                                                               msgtype=MessageType.TEXT
+                                                                               )
+                                self.db.save_message(room_name, room_id, msg_event_id, r.get("id"), content)
 
-                    elif r.get('type') == 'delete':
-                        msg_id = r.get("id")
-                        # self.log.info(f'delete message: {msg_id} from room {room_id} / evt: {evt_id}')
+                        elif r.get('type') == 'delete':
+                            msg_id = r.get("id")
+                            # self.log.info(f'delete message: {msg_id} from room {room_id} / evt: {evt_id}')
 
-                        result = self.db.get_evt_by_message_id(msg_id)
-                        room_id = result[0]
-                        evt_id = result[1]
+                            result = self.db.get_evt_by_message_id(msg_id)
+                            room_id = result[0]
+                            evt_id = result[1]
 
-                        await self.client.redact(
-                            room_id=room_id,
-                            reason="deleted with MessageRelayLite",
-                            event_id=evt_id
-                        )
+                            await self.client.redact(
+                                room_id=room_id,
+                                reason="deleted with MessageRelayLite",
+                                event_id=evt_id
+                            )
 
-                        self.db.message_set_deleted(msg_id)
+                            self.db.message_set_deleted(msg_id)
+                except ConnectionClosed:
+                    self.log.warning(f'websocket connection {api_uri} lost. reconnecting...')
+                    continue
 
     @command.new("mrroom", aliases=["messagerelay", "mr-room"],
                  help="Admincommand to set rooms for the messagerelay(light) !mrroom <room_name>.")
